ff97728298f04967136b47a55acf4c17890651d1
chike4190 <chikeawuzie@gmail.com>
2022-10-27 15:33:35 -0700

    Determination not Magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-/**
- * main - uses magic to print a[2] = 98
- */
+
 int main(void)
 {
 	int n;
@@ -13,6 +11,7 @@ int main(void)
 	/*
 	 * write your line of code here...
 	 * Remember:
+	 * main - uses magic to  print a[2] = 98
 	 * - you are not allowed to use a
 	 *   - you are not allowed to modify p
 	 *   - only one statement
