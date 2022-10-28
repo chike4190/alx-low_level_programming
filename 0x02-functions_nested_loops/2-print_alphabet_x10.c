af1bb1aa71fa9584f1f30d34c3e70806ad94c9ae
chike4190 <chikeawuzie@gmail.com>
2022-10-28 13:57:52 -0700

    Alpha x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints lowercase Alphabets x10
+ */
+void print_alphabet_x10(void)
+{
+	int w;
+	int j;
+
+	while (x++ <= 9)
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
