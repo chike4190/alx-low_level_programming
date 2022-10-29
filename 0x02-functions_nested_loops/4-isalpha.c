70dbda1833516f0b067fa6dd7a70e962d740a266
chike4190 <chikeawuzie@gmail.com>
2022-10-28 21:50:29 -0700

    Check Alphaness

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: char to be checked
+ * Return: 1 if true, otherwise 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
