ddbbc3060249833bc0b86a8d015f1ade89f5cc3c
chike4190 <chikeawuzie@gmail.com>
2022-10-30 04:25:48 -0700

    Print 0-9 cd alx-low_level_programming, 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,13 +3,14 @@
  * print_numbers - print 0 - 9 !2, 4
  * Return: void
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(c);
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
 	}
 	_putchar('\n');
 }
